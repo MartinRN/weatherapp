76c423e3527cc95b3436f1926332acb97fd238e3
martin.giot <martingiot.pro@gmail.com>
2021-01-14 17:31:37 +0100

    handle exception

--- src/Service/OpenWeatherMap.php
+++ src/Service/OpenWeatherMap.php
@@ -5,7 +5,6 @@ namespace App\Service;
 
 
 use App\Entity\City;
-use Symfony\Component\HttpClient\ScopingHttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
@@ -25,18 +24,22 @@ class OpenWeatherMap
         $defaultUrl = "http://api.openweathermap.org/data/2.5/weather?APPID=".$api_key."&units=metric";
         $defaultUrl.= "&q=".$city;
         $response = $this->openweatherchannel->request('GET', $defaultUrl);
-        $data = $response->getContent();
-        $data = json_decode($data, true);
-
-        $cityWeather = New City();
-        $cityWeather->setId(1);
-        $cityWeather->setCountry($data['sys']['country']);
-        $cityWeather->setIcon('https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/'.$data['weather'][0]['icon'].'.svg');
-        $cityWeather->setName($city);
-        $cityWeather->setTemperature($data['main']['temp']);
-        $cityWeather->setWeather($data['weather'][0]['description']);
-
+        $status = $response->getStatusCode();
+        $cityWeather = null;
+        if($status == '200'){
+            $data = $response->getContent();
+            $data = json_decode($data, true);
+            $cityWeather = New City();
+            $cityWeather->setId(1);
+            $cityWeather->setCountry($data['sys']['country']);
+            $cityWeather->setIcon('https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/'.$data['weather'][0]['icon'].'.svg');
+            $cityWeather->setName($city);
+            $cityWeather->setTemperature($data['main']['temp']);
+            $cityWeather->setWeather($data['weather'][0]['description']);
+            return $cityWeather;
+        }
         return $cityWeather;
+        
     }
 
 }
